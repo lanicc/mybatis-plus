71bbe4e20027cbf1345ed3bb386259d87d64d4de
lan <1728209643@qq.com>
2020-09-23 15:22:01 +0800

    sqlserver不支持批量操作后获取主键 id https://github.com/Microsoft/mssql-jdbc/issues/245

--- lan-test/src/test/java/org/lanettiesso/lan/test/mssql/MsSqlTest.java
+++ lan-test/src/test/java/org/lanettiesso/lan/test/mssql/MsSqlTest.java
@@ -4,25 +4,67 @@ import org.junit.Test;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Arrays;
 
 /**
  * @author lan
  * @date 2020/9/23
  */
 public class MsSqlTest {
+    DataSource dataSource = dataSource();
 
+    @Test
+    public void generatedKey() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Statement stmt = connection.createStatement();
+        String SQL = "insert into sys_role(role_name) values ('x')";
+        int count = stmt.executeUpdate(SQL, Statement.RETURN_GENERATED_KEYS);
+        ResultSet rs = stmt.getGeneratedKeys();
+
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        if (rs.next()) {
+            do {
+                for (int i = 1; i <= columnCount; i++) {
+                    String key = rs.getString(i);
+                    System.out.println("KEY " + i + " = " + key);
+                }
+            } while (rs.next());
+        } else {
+            System.out.println("NO KEYS WERE GENERATED.");
+        }
+    }
+
+    /**
+     * <a href="https://github.com/Microsoft/mssql-jdbc/issues/245">Github issue</a>
+     *
+     * @throws SQLException SQLException
+     */
     @Test
     public void batchTest() throws SQLException {
         DataSource dataSource = dataSource();
         Connection connection = dataSource.getConnection();
         Statement stmt = connection.createStatement();
-        stmt.addBatch("INSERT INTO TestTable (Col2, Col3) VALUES ('X', 100)");
-        stmt.addBatch("INSERT INTO TestTable (Col2, Col3) VALUES ('Y', 200)");
-        stmt.addBatch("INSERT INTO TestTable (Col2, Col3) VALUES ('Z', 300)");
+        stmt.addBatch("insert into sys_role(role_name) values ('x')");
+        stmt.addBatch("insert into sys_role(role_name) values ('x')");
+        stmt.addBatch("insert into sys_role(role_name) values ('x')");
+        stmt.addBatch("insert into sys_role(role_name) values ('x')");
         int[] updateCounts = stmt.executeBatch();
+        System.out.println(Arrays.toString(updateCounts));
+        ResultSet rs = stmt.getGeneratedKeys();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        if (rs.next()) {
+            do {
+                for (int i = 1; i <= columnCount; i++) {
+                    String key = rs.getString(i);
+                    System.out.println("KEY " + i + " = " + key);
+                }
+            } while (rs.next());
+        } else {
+            System.out.println("NO KEYS WERE GENERATED.");
+        }
     }
 
     public DataSource dataSource() {
